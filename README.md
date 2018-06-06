928e9fda83f2b148edbcf02c4d02fd1d280344d9
grial <grial@isbel.com.uy>
2018-06-06 02:01:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Utility functions for creating and deploying lambda functions to aws
+
+### Usage
+##### Initialize project
+```
+$source lambda_manager.bash
+$init_lambda <nombre-proyecto>
+```
+
+##### Deploy package from virtual enviroment
+```
+// Inside <nombre-proyecto> folder
+$deploy_lambda <execution-role-arn>
+```
+
---
+++ lambda_manager.bash
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+# description:  Utility functions for creating a new project in AWS lambda
+#               python 2.7 in a virtual enviroment and deploying it.
+# author: Gaston Rial
+
+function init_lambda()
+{
+
+  # Start virtualenv and chande to that folder
+  echo -e "Creating project ${PWD}/${1} ..."
+  virtualenv ./${1} && cd $_
+  source bin/activate
+
+  # Create template for lambda
+  touch lambda_function.py
+  echo -e "# Import dependencies\n#import boto3" >> lambda_function.py
+  echo -e "\n\ndef lambda_handler(event, context):" >> lambda_function.py
+  echo -e "# mind identation" >> lambda_function.py
+  
+  # Get Boto3
+  pip install boto3
+
+  echo -e "\nReady to begin, please open the file lambda_function.py"
+
+}
+
+function deploy_lambda()
+{
+
+  echo -e "\nStarting deployment"
+
+  # Put together every file on the deployment package
+  mkdir build_sources
+  cp lambda_function.py build_sources
+  cp -r lib/python*/* build_sources
+  cd build_sources && zip -r lambda_function.zip *
+
+  # Deploy to AWS
+  aws lambda create-function \
+--function-name lambda_function  \
+--zip-file fileb://${PWD}/lambda_function.zip \
+--role ${1} \
+--handler lambda_function.lambda_handler \
+--runtime python2.7 \
+--timeout 60 \
+--memory-size 1024 
+
+  echo -e "\nlambda_function.py deployed!\nCheck out AWS"
+  
+}
